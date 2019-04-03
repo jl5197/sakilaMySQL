f8e11b048928a508d52e96e5ad58a5b44ee11fc4
Jiahao Liang (Chahol) <46583647+jl5197@users.noreply.github.com>
2019-04-03 03:16:40 -0400

    Update sakila_db_analysis_MySQL.sql

--- sakila_db_analysis_MySQL.sql
+++ sakila_db_analysis_MySQL.sql
@@ -1,44 +1,44 @@
 USE sakila
 
--- 1a 
+-- 1a Display the first and last names of all actors from the table actor
 SELECT first_name, last_name FROM actor; 
 
--- 1b
+-- 1b Display the first and last name of each actor in a single column in upper case letters.
 SELECT CONCAT(first_name, ' ', last_name) AS 'Actor Name' FROM actor;
 
--- 2a
+-- 2a Find the ID number, first name, and last name of an actor whose first name is "Joe."
 SELECT actor_id, first_name, last_name FROM actor WHERE first_name = 'JOE';
 
--- 2b
+-- 2b All actors whose last name contain the letters 'GEN'
 SELECT actor_id, first_name, last_name FROM actor WHERE last_name LIKE '%GEN%';
 
--- 2c
+-- 2c All actors whose last names contain the letters 'LI'
 SELECT actor_id, last_name, first_name FROM actor WHERE last_name LIKE '%LI%';
 
--- 2d
+-- 2d Display the country_id and country columns of the following countries: 'Afghanistan', 'Bangladesh', and 'China'
 SELECT c.country_id, c.country FROM country c 
 WHERE c.country IN ('Afghanistan', 'Bangladesh', 'China'); 
 
--- 3a
+-- 3a Create a column in the table actor called description and use the data type BLOB
 ALTER TABLE actor
 ADD COLUMN description BLOB AFTER last_name;
 
--- 3b
+-- 3b Delete the description column
 ALTER TABLE actor
 DROP COLUMN description; 
 
--- 4a
+-- 4a List the last names of actors, as well as how many actors have that last name.
 SELECT last_name, COUNT(last_name) AS 'Total Number of Actors with THIS last_name' 
 FROM actor GROUP BY last_name;
 
--- 4b
+-- 4b List last names of actors and the number of actors who have that last name (only for names that are shared by at least two actors)
 SELECT last_name, COUNT(last_name) AS 'Total Number of Actors with THIS last_name' 
 FROM actor GROUP BY last_name HAVING COUNT('Total Number of Actors with THIS last_name') > 1; 
 
--- 4c
+-- 4c Change the actor 'HARPO WILLIAMS' to 'GROUCHO WILLIAMS'
 UPDATE actor SET first_name = 'HARPO' WHERE last_name = 'WILLIAMS' AND first_name = 'GROUCHO'; 
 
--- 4d
+-- 4d 
 SET SQL_SAFE_UPDATES = 0;
 UPDATE actor SET first_name = 'GROUCHO' WHERE first_name = 'HARPO'; 
 SET SQL_SAFE_UPDATES = 1;
